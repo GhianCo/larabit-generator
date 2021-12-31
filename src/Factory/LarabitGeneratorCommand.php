a637d82a7ce40d79fcd15411161c6a67b43af4d8
GhianCo <ghiancorx@gmail.com>
2021-12-31 00:41:04 -0500

    cambios para generator larabit

--- src/Factory/LarabitGeneratorCommand.php
+++ src/Factory/LarabitGeneratorCommand.php
@@ -26,46 +26,16 @@ class LarabitGeneratorCommand extends Command
                 'database',
                 InputOption::VALUE_REQUIRED,
                 'Cual es la base de datos de conexión?.'
-            )->addOption(
-                'host',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'Cual es el host de conexión?.'
-            )->addOption(
-                'user',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'Cual es el usuario de conexión?.'
-            )->addOption(
-                'pass',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'Cual es la clave de conexión?.'
-            )->addOption(
-                'port',
-                null,
-                InputOption::VALUE_OPTIONAL,
-                'Cual es la puerto de conexión?.'
             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->validateOptionsAndArgs($input);
         $dbConn = $this->container->get('db');
         $database = $input->getArgument('database');
         $generator = new LarabitGeneratorService($dbConn, $database);
         $generator->generateStructure();
         $output->writeln('Success - Se generaron los recursos de la base de datos: ' . $database);
         return 0;
-    }
-
-    protected function validateOptionsAndArgs(InputInterface $input)
-    {
-        if ($input->getArgument('database')) $_SERVER['DB_NAME'] = $input->getArgument('database');
-        if ($input->getOption('host')) $_SERVER['DB_HOST'] = $input->getOption('host');
-        if ($input->getOption('user')) $_SERVER['DB_USER'] = $input->getOption('user');
-        if ($input->getOption('pass')) $_SERVER['DB_PASS'] = $input->getOption('pass');
-        if ($input->getOption('port')) $_SERVER['DB_PORT'] = $input->getOption('port');
     }
 }
