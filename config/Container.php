a5025abff22ed24e264ce83742c4f2fa3c34c803
GhianCo <ghiancorx@gmail.com>
2021-12-30 22:19:25 -0500

    first commit

--- config/Container.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Pimple\Container;
-use Pimple\Psr11\Container as Psr11Container;
-use Slim\Factory\AppFactory;
-
-$container = new Container();
-
-return AppFactory::create(new ResponseFactory(), new Psr11Container($container));
